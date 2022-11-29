4ae4de616b58243c1b6f8c3fe52bf8c58433c467
Chery <cherylene40@gmail.com>
2022-11-29 22:34:08 +0700

    Modify database again

--- Database/selecting_data.sql
+++ Database/selecting_data.sql
@@ -1,9 +1,11 @@
 -- SELECT GABUNGAN HISTORY & PEMBELIAN
 SELECT * FROM history AS hs 
 JOIN Pembelian AS pb 
-ON hs.id_pembelian = pb.id_pembelian WHERE id_user = xxx
+ON hs.id_pembelian = pb.id_pembelian 
+WHERE id_user = xxx
 
 -- SELECT GABUNGAN FILM & BIOSKOP
 SELECT * FROM film AS fl
 JOIN bioskop AS bs
 ON bs.id_bioskop = fl.id_bioskop
+WHERE id_film = xxx
